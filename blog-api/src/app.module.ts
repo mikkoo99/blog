e3853048119b8b69e738ca179fd7b10995e1fd3e
Mateusz Żupa <yetiasg@gmail.com>
2022-03-20 12:32:15 +0100

    Inject appropriate config files

--- blog-api/src/app.module.ts
+++ blog-api/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PostModule } from './post/post.module';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
-  imports: [PostModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ENV ? `${ENV}.env` : `development.env`,
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+    PostModule,
+  ],
 })
 export class AppModule {}
